1762ae3104983d00eec489b717ee80bebc6b7ce6
shani00137 <shani00137@gmail.com>
2025-12-18 19:55:30 +0400

    sql lite added

---
+++ HumanDetection/SQLite/Database.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SQLite
+{
+    public static class Database
+    {
+        public static void Initialize()
+        {
+            Directory.CreateDirectory(DbPaths.AppFolder);
+
+            using var con = new SqliteConnection(GetConnectionString());
+            con.Open();
+
+            // Good defaults (WAL helps concurrency)
+            using (var cmd = con.CreateCommand())
+            {
+                cmd.CommandText = @"
+                                    PRAGMA journal_mode = WAL;
+                                    PRAGMA synchronous = NORMAL;
+
+                                    CREATE TABLE IF NOT EXISTS Rules (
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        RuleType TEXT NOT NULL, 
+                                        Name TEXT NOT NULL,
+                                        Json TEXT NOT NULL,
+                                        CreatedAt TEXT NOT NULL
+                                    );
+
+
+                                    CREATE TABLE IF NOT EXISTS Settings (
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        ComPort TEXT NOT NULL, 
+                                        MoxIP TEXT NOT NULL,
+                                        ConfidenceLevel TEXT NOT NULL,
+                                    );";
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private static string GetConnectionString()
+        {
+            var csb = new SqliteConnectionStringBuilder
+            {
+                DataSource = DbPaths.DbFile,
+                Mode = SqliteOpenMode.ReadWriteCreate,
+                Cache = SqliteCacheMode.Shared
+            };
+            return csb.ToString();
+        }
+    }
+
+}
+
---
+++ HumanDetection/SQLite/DbPaths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SQLite
+{
+    public static class DbPaths
+    {
+        public static string AppFolder =>
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+                "AdPort",
+                "AdPortDB");
+
+        public static string DbFile =>
+            Path.Combine(AppFolder, "app.db");
+    }
+}
